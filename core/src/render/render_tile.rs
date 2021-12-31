5065a0edee35ca765125883b3405b8c0d550b61b
Travis Finkenauer <tmfinken@gmail.com>
2021-12-31 02:34:55 -0800

    Render warp points

--- core/src/render/render_tile.rs
+++ core/src/render/render_tile.rs
@@ -2,12 +2,46 @@ use macroquad::prelude as mq;
 
 use crate::prelude::*;
 
+const GRID_WIDTH: f32 = Tile::CELL_GRID_WIDTH as f32;
+const GRID_HALF_WIDTH: f32 = 0.5 * GRID_WIDTH;
+const CELL_WIDTH: f32 = 1.0;
+const CELL_HALF_WIDTH: f32 = 0.5 * CELL_WIDTH;
+
+fn render_timer(render: &RenderState, x: f32, y: f32) {
+    let x_left = x + 0.25;
+    let x_right = x + 0.75;
+    let y_top = y + 0.2;
+    let y_bottom = y + 0.8;
+
+    let points = [
+        mq::Vec2::new(x_left, y_top),
+        mq::Vec2::new(x_right, y_top),
+        mq::Vec2::new(x_left, y_bottom),
+        mq::Vec2::new(x_right, y_bottom),
+        mq::Vec2::new(x_left, y_top),
+    ];
+    shape::draw_connected_line(
+        points.iter().copied(),
+        render.theme.wall_thickness,
+        render.theme.timer_color,
+    );
+}
+
+fn render_warp(render: &RenderState, x: f32, y: f32, pawn: Pawn) {
+    let center = mq::Vec2::new(x + CELL_HALF_WIDTH, y + CELL_HALF_WIDTH);
+
+    let angles = (0..8)
+        .map(|x| x as f32 * 2.0 * std::f32::consts::PI / 8.0)
+        .cycle();
+    let radii = (0..20).map(|x| x as f32 * CELL_HALF_WIDTH * 0.8 / 20.0);
+    let points = angles
+        .zip(radii)
+        .map(|(angle, radius)| mq::polar_to_cartesian(radius, angle) + center);
+    shape::draw_connected_line(points, render.theme.warp_thickness, pawn.as_color(render));
+}
+
 impl Render for Tile {
     fn render(&self, pos: mq::Vec2, render: &RenderState) {
-        const GRID_WIDTH: f32 = Tile::CELL_GRID_WIDTH as f32;
-        const GRID_HALF_WIDTH: f32 = GRID_WIDTH / 2.0;
-        const CELL_WIDTH: f32 = 1.0;
-
         mq::draw_rectangle(
             pos.x + -GRID_HALF_WIDTH,
             pos.y + -GRID_HALF_WIDTH,
@@ -61,29 +95,11 @@ impl Render for Tile {
                 let x = -GRID_HALF_WIDTH + col_idx * CELL_WIDTH;
 
                 match cell {
-                    TileCell::TimerFlip(_) => {
-                        let x_left = x + 0.25;
-                        let x_right = x + 0.75;
-                        let y_top = y + 0.2;
-                        let y_bottom = y + 0.8;
-
-                        let points = [
-                            mq::Vec2::new(x_left, y_top),
-                            mq::Vec2::new(x_right, y_top),
-                            mq::Vec2::new(x_left, y_bottom),
-                            mq::Vec2::new(x_right, y_bottom),
-                            mq::Vec2::new(x_left, y_top),
-                        ];
-                        shape::draw_connected_line(
-                            &points,
-                            render.theme.wall_thickness,
-                            render.theme.timer_color,
-                        );
-                    }
+                    TileCell::TimerFlip(_) => render_timer(render, x, y),
+                    TileCell::Warp(pawn) => render_warp(render, x, y, pawn),
 
                     //TileCell::Camera(_) => todo!(),
                     //TileCell::FinalExit(_) => todo!(),
-                    //TileCell::Warp(_) => todo!(),
                     //TileCell::Loot(_) => todo!(),
                     TileCell::Empty => (),
                     _ => (),
--- core/src/render/shape.rs
+++ core/src/render/shape.rs
@@ -1,11 +1,12 @@
+use itertools::Itertools;
 use macroquad::prelude as mq;
 
-pub fn draw_connected_line(points: &[mq::Vec2], thickness: f32, color: mq::Color) {
-    for s in points.windows(2) {
-        let (a, b) = match s {
-            [a, b] => (a, b),
-            _ => unreachable!("should only get 2 elements in window"),
-        };
-        mq::draw_line(a.x, a.y, b.x, b.y, thickness, color)
+pub fn draw_connected_line<I: Iterator<Item = mq::Vec2>>(
+    points: I,
+    thickness: f32,
+    color: mq::Color,
+) {
+    for (a, b) in points.tuple_windows() {
+        mq::draw_line(a.x, a.y, b.x, b.y, thickness, color);
     }
 }
